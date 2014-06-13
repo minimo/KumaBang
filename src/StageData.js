61a179c1b2523e1e317efceb4fbca4480396d3b3
minimo <minimo.mobile@gmail.com>
2014-06-13 15:21:44 +0900

    add stage data

--- src/StageData.js
+++ src/StageData.js
@@ -15,11 +15,12 @@
 //8:スタート位置
 //9:ゴール位置
 
+kumabang.numStage = 0;
 kumabang.stageData = [];
 
 //ステージ１
 tm.define("kumabang.Stage1", {
-	number: 0,              //ステージ番号
+	number: 1,              //ステージ番号
 	needConditon: false,    //クリア条件有りフラグ
 	clearOK: true,          //条件を満たしているか
 
@@ -50,6 +51,149 @@ tm.define("kumabang.Stage1", {
 		return true;
 	},
 });
-kumabang.stageData[0] = kumabang.Stage1();
+kumabang.stageData[kumabang.numStage] = kumabang.Stage1();
+kumabang.numStage++;
 
+//ステージ２
+tm.define("kumabang.Stage2", {
+	number: 2,              //ステージ番号
+	needConditon: false,    //クリア条件有りフラグ
+	clearOK: true,          //条件を満たしているか
 
+	//マップ初期位置配列
+	map: [
+        [9,4,5,4,5],
+        [6,3,7,2,2],
+        [4,7,4,7,2],
+        [2,4,3,5,2],
+        [6,7,6,7,15]
+	],
+
+	//アイテム初期位置配列
+	item: [
+        [8,6,6,0,0],
+        [6,1,6,0,0],
+        [0,6,0,0,0],
+        [0,0,6,0,1],
+        [0,0,0,0,9],
+	],
+
+    //初期化
+	init: function() {
+	},
+
+	//クリア条件チェック
+	checkCondition: function(scene) {
+		return true;
+	},
+});
+kumabang.stageData[kumabang.numStage] = kumabang.Stage2();
+kumabang.numStage++;
+
+//ステージ３
+tm.define("kumabang.Stage3", {
+	number: 3,              //ステージ番号
+	needConditon: false,    //クリア条件有りフラグ
+	clearOK: true,          //条件を満たしているか
+
+	//マップ初期位置配列
+	map: [
+        [4,1,1,1,5],
+        [2,4,1,5,2],
+        [2,6,1,3,7],
+        [2,4,5,6,5],
+        [11,15,6,1,7],
+	],
+
+	//アイテム初期位置配列
+	item: [
+        [6,0,0,0,0],
+        [6,0,0,2,0],
+        [6,0,0,0,0],
+        [6,6,6,0,0],
+        [8,9,6,0,0],
+	],
+
+    //初期化
+	init: function() {
+	},
+
+	//クリア条件チェック
+	checkCondition: function(scene) {
+		return true;
+	},
+});
+kumabang.stageData[kumabang.numStage] = kumabang.Stage3();
+kumabang.numStage++;
+
+//ステージ４
+tm.define("kumabang.Stage4", {
+	number: 4,              //ステージ番号
+	needConditon: false,    //クリア条件有りフラグ
+	clearOK: true,          //条件を満たしているか
+
+	//マップ初期位置配列
+	map: [
+        [4,5,9,4,5],
+        [2,2,6,7,2],
+        [6,3,5,4,7],
+        [4,3,3,3,5],
+        [6,7,15,6,7],
+	],
+
+	//アイテム初期位置配列
+	item: [
+        [0,0,8,6,6],
+        [0,0,0,6,6],
+        [0,1,6,1,0],
+        [0,0,0,0,0],
+        [0,0,9,0,0],
+	],
+
+    //初期化
+	init: function() {
+	},
+
+	//クリア条件チェック
+	checkCondition: function(scene) {
+		return true;
+	},
+});
+kumabang.stageData[kumabang.numStage] = kumabang.Stage4();
+kumabang.numStage++;
+
+//ステージ５
+tm.define("kumabang.Stage5", {
+	number: 5,              //ステージ番号
+	needConditon: false,    //クリア条件有りフラグ
+	clearOK: true,          //条件を満たしているか
+
+	//マップ初期位置配列
+	map: [
+        [13,4,5,4,5],
+        [2,2,6,3,7],
+        [4,7,3,6,5],
+        [6,7,4,1,7],
+        [6,5,6,1,10],
+	],
+
+	//アイテム初期位置配列
+	item: [
+        [9,0,0,6,1],
+        [0,0,0,0,6],
+        [2,0,5,0,0],
+        [6,0,0,0,0],
+        [1,6,0,6,8],
+	],
+
+    //初期化
+	init: function() {
+	},
+
+	//クリア条件チェック
+	checkCondition: function(scene) {
+		return true;
+	},
+});
+kumabang.stageData[kumabang.numStage] = kumabang.Stage5();
+kumabang.numStage++;
